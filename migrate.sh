b8ecbc26b1b6eb48785f10d95c1ed5af93089ed6
Jakub Peleska <pelesjak@fel.cvut.cz>
2024-01-30 20:03:00 +0100

    Change source of dumps backups

--- migrate.sh
+++ migrate.sh
@@ -6,7 +6,7 @@ FOLDER=./datasets
 
 
 if [ -z "$(ls -A $FOLDER)" ]; then
-  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
+  wget -O $FILENAME https://link.storjshare.io/s/jug2r2aqggp6g3rcp6d5p4od6sxq/datasets%2Fdatasets.zip?download=1
 
   unzip $FILENAME
 fi
