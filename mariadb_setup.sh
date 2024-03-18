d2cd133465cc1c92b07b6caf98b2a635db0b237c
Jakub Peleska <pelesjak@fel.cvut.cz>
2024-03-18 16:22:17 +0100

    Adjust mariadb setup script

--- mariadb_setup.sh
+++ mariadb_setup.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+# Remove remote access for root user
+docker exec -i dataset-db-mariadb mariadb --password=password -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+
 # User name and host
 GUEST_USER="guest@'%'"
 GUEST_PASSWORD="ctu-relational"
 GUEST_GRANTS="SELECT, SHOW VIEW"
 
-docker exec -i dataset-db-mariadb mariadb -e "CREATE OR REPLACE USER $GUEST_USER IDENTIFIED BY '$GUEST_PASSWORD';"
+# Create Guest user
+docker exec -i dataset-db-mariadb mariadb --password=password -e "CREATE OR REPLACE USER $GUEST_USER IDENTIFIED BY '$GUEST_PASSWORD';"
 
 # Get the list of databases, excluding the ones you don't want to grant access to
-databases=$(docker exec -i dataset-db-mariadb mariadb -e "SHOW DATABASES;" | grep -v -E 'Database|information_schema|performance_schema|mysql|test|test_db')
+databases=$(docker exec -i dataset-db-mariadb mariadb --password=password -e "SHOW DATABASES;" | grep -v -E 'Database|information_schema|performance_schema|mysql|test|test_db')
 
 # Loop through and grant SELECT on each database
 for db in $databases; do
     echo "Granting $GUEST_GRANTS on $db to $GUEST_USER"
-    docker exec -i dataset-db-mariadb mariadb -e "GRANT $GUEST_GRANTS ON \`$db\`.* TO $GUEST_USER;"
+    docker exec -i dataset-db-mariadb mariadb --password=password -e "GRANT $GUEST_GRANTS ON \`$db\`.* TO $GUEST_USER;"
 done
 
-docker exec -i dataset-db-mariadb mariadb -e "FLUSH PRIVILEGES;"
+docker exec -i dataset-db-mariadb mariadb --password=password -e "FLUSH PRIVILEGES;"
 
 echo "Permissions granted."
